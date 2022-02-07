7e79d7d2758a906fc1427f3a332da7e2502212a9
James <git@umimaso.com>
2022-02-07 15:16:24 +0000

    init
    Initial Leiningen template with Luminus

---
+++ env/prod/clj/clojure_store/env.clj
@@ -0,0 +1,11 @@
+(ns clojure-store.env
+  (:require [clojure.tools.logging :as log]))
+
+(def defaults
+  {:init
+   (fn []
+     (log/info "\n-=[clojure-store started successfully]=-"))
+   :stop
+   (fn []
+     (log/info "\n-=[clojure-store has shut down successfully]=-"))
+   :middleware identity})
