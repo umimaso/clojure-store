f396bfeb179a30ae55ad03b2e91878a5ff098798
James <git@umimaso.com>
2022-02-14 22:03:07 +0000

    feat: add schema validation for t-shirt order form
    - Add schema validation for new order request
    - Add id, and name field attributes to form inputs for handling the
      request in the backend
    - Add new SQL query to be used within schema validation

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -11,3 +11,9 @@ SELECT
     tshirt_option_type_id,
     tshirt_option_name
 FROM tshirt_option WHERE is_deleted = 0;
+
+-- :name get-tshirt-option-names-for-type :? :*
+-- :doc retrieve values for a given tshirt option type
+SELECT
+    tshirt_option_name
+FROM tshirt_option WHERE tshirt_option_type_id = :type_id AND is_deleted = 0;
--- src/clj/clojure_store/routes/order.clj
+++ src/clj/clojure_store/routes/order.clj
@@ -5,25 +5,80 @@
    [clojure.java.io :as io]
    [clojure-store.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [clojure.string :as str]
+   [struct.core :as st]
+   [clojure.tools.logging :as log]))
 
+;
+; Helpers
+;
+
+; Unused, hardcoded order schema
+;(def order-schema
+;  {:full_name [st/required st/string]
+;   :email [st/required st/email]
+;   :phone_number [st/required st/string]
+;   :shipping_address [st/required st/string]
+;   :delivery_details [st/string]
+;   :quantity [st/required st/integer-str]
+;   :size [st/required [st/member ["Small" "Medium" "Large" "Extra Large" "Extra Extra Large"]]]
+;   :colour [st/required [st/member ["Red" "Orange" "Yellow" "Green" "Cyan" "Blue" "Violet"]]]
+;   :quality [st/required [st/member ["Standard" "Supreme"]]]
+;   :image [st/required [st/member ["Clojure" "Python" "Rust" "Custom"]]]})
+
+(defn validate-order
+  [params]
+  (first (st/validate
+          params
+          (merge
+           {:full_name [st/required st/string]
+            :email [st/required st/email]
+            :phone_number [st/required st/string]
+            :shipping_address [st/required st/string]
+            :delivery_details [st/string]
+            :quantity [st/required st/integer-str]}
+          ; Dynamic schema generated from the database
+           (zipmap
+            (for
+             [{:keys [tshirt_option_type_name]} (db/get-tshirt-option-types)]
+              (keyword (str/lower-case tshirt_option_type_name)))
+            (for
+             [{:keys [id]} (db/get-tshirt-option-types)]
+              [st/required [st/member (for
+                                       [{:keys [tshirt_option_name]} (db/get-tshirt-option-names-for-type {:type_id id})]
+                                        tshirt_option_name)]]))))))
+
+;
+; Routes
+;
 (defn order-form [request]
   (layout/render
    request
-   "order.html"
+   "order-form.html"
    (merge
     {:option-types (db/get-tshirt-option-types)}
     {:options (db/get-tshirt-options)})))
 
-(defn new-order [request]
+; TODO: Display errors on order page
+; TODO: Add new order to database if order is successful
+; TODO: Display order confirmation after adding order to database
+(defn new-order [{:keys [params]}]
+  (if-let [errors (validate-order params)]
+    (-> (response/found "/order")
+        (log/debug errors))
+    (-> (response/found "/order/confirmation"))))
+
+; TODO: Display confirmed order id in template
+(defn order-confirmation [request]
   (layout/render
    request
-   "order-confirmation.html"
-   (response/found "/order/confirmation")))
+   "order-confirmation.html"))
 
 (defn order-routes []
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
    ["/order" {:get order-form
-              :post new-order}]])
+              :post new-order}]
+   ["/order/confirmation" {:get order-confirmation}]])
