31639a0910f275bb7481a54d0b9704b631ba9ad8
James <git@umimaso.com>
2022-02-25 14:48:25 +0000

    refactor: order page routes
    - Refactor order page route code
    - Rename order-form.html to order.html

--- src/clj/clojure_store/routes/order.clj
+++ src/clj/clojure_store/routes/order.clj
@@ -27,16 +27,16 @@
             :delivery_details [st/string]
             :quantity [st/required st/integer-str] ; TODO: Prevent quantity accepting negative values
             :custom_image_url [st/string]}
-          ; Dynamic schema generated from the database
+           ; Dynamic schema generated from the database
            (zipmap
             (for
              [{:keys [tshirt_option_type_name]} (db/get-option-types)]
               (keyword (str/lower-case tshirt_option_type_name)))
             (for
              [{:keys [id]} (db/get-option-types)]
-              [st/required [st/member (for
-                                       [{:keys [tshirt_option_name]} (db/get-option-names-for-type {:type_id id})]
-                                        tshirt_option_name)]]))))))
+              [st/required [st/member
+                            (for [{:keys [tshirt_option_name]} (db/get-option-names-for-type {:type_id id})]
+                              tshirt_option_name)]]))))))
 
 ; If the custom image option was selected, ensure that the field isn't empty
 (defn validate-order-image
@@ -45,7 +45,7 @@
   (if (= (get params :image) "Custom")
     ; Check if custom image url is blank
     (if-let [custom-image (str/blank? (get params :custom_image_url))]
-      (hash-map :custom_image_url "this field is mandatory when custom image is selected"))))
+      {:custom_image_url "this field is mandatory when custom image is selected"})))
 
 ; Validate that the order options selected are in stock
 (defn validate-order-stock
@@ -71,7 +71,7 @@
                            ; Compare stock against quantity in order
                            (if (> quantity stock)
                              ; Higher quantity requested for option than in stock
-                             (hash-map :tshirt_option "Item(s) selected not in stock")))))]
+                             {:tshirt_option "Item(s) selected not in stock"}))))]
       (if (not= result {})
         result))))
 
@@ -79,10 +79,9 @@
 ; Routes
 ;
 (defn order-form [{:keys [flash] :as request}]
-  (log/debug flash)
   (layout/render
    request
-   "order-form.html"
+   "order.html"
    (merge
     {:option-types (db/get-option-types)}
     {:options (db/get-options)}
@@ -94,67 +93,62 @@
             (validate-order-schema params)
             (validate-order-image params)
             (validate-order-stock params))]
-    (->
-     (response/found "/order")
-     (assoc :flash (assoc params :errors errors)))
-    (do
-      ; Validation was successful, add the order to the database
-      (if-let [new-order (db/create-order!
-                          (merge
-                           params
-                           (hash-map
-                            :price (*
-                                    (Integer/parseInt (get params :quantity))
-                                    (Double/parseDouble
-                                     (get
-                                      (db/get-price-for-quality {:quality (get params :quality)})
-                                      :tshirt_option_value))),
-                            :payment_success true,
-                            :delivered false)))]
-        (do
-          ; Add each tshirt option in the order to the database for the order id created
-          ; Decrease stock for each tshirt option
-          (doseq [{:keys [tshirt_option_type_name id]} (db/get-option-types)]
-            (db/create-order-option!
-             (hash-map
-              :order_id (get new-order :id),
-              :tshirt_option_type_id id,
-              :tshirt_option_id (get
-                                 (db/get-option-for-type-and-name
-                                  {:type_id id,
-                                   :option_name (get params (keyword (str/lower-case tshirt_option_type_name)))})
-                                 :id),
-              :tshirt_option_value (if-let [custom-image
-                                            (if (= tshirt_option_type_name "Image")
-                                              (if (= (get params :image) "Custom")
-                                                (get params :custom_image_url)))]
-                                     custom-image
-                                     (get
-                                      (db/get-option-for-type-and-name
-                                       {:type_id id,
-                                        :option_name (get params (keyword (str/lower-case tshirt_option_type_name)))})
-                                      :tshirt_option_value))))
-            (let [option_id
-                  (get
-                   (db/get-option-for-type-and-name
-                    {:type_id id,
-                     :option_name (get params (keyword (str/lower-case tshirt_option_type_name)))})
-                   :id)]
-              (if-let [stock
-                       (get
-                        (db/get-stock-for-option-id
-                         {:option_id option_id})
-                        :stock_count)]
-                (db/update-stock-for-option-id!
-                 (hash-map
-                  :stock_count (-
-                                stock
-                                (Integer/parseInt (get params :quantity))),
-                  :option_id option_id)))))))
+    ; Invalid order, reload page with error messages
+    (assoc (response/found "/order") :flash (assoc params :errors errors))
 
-      (->
-       (response/found "/order")
-       (assoc :flash (assoc params :success "true"))))))
+    ; Validation was successful, add the order to the database
+
+    (let [new-order (db/create-order!
+                     (merge
+                      params
+                      {:price (*
+                               (Integer/parseInt (get params :quantity))
+                               (Double/parseDouble
+                                (get
+                                 (db/get-price-for-quality {:quality (get params :quality)})
+                                 :tshirt_option_value))),
+                       :payment_success true,
+                       :delivered false}))]
+
+      ; Add each tshirt option in the order to the database for the order id created
+      ; Decrease stock for each tshirt option
+      (doseq [{:keys [tshirt_option_type_name id]} (db/get-option-types)]
+        (db/create-order-option!
+         {:order_id (get new-order :id),
+          :tshirt_option_type_id id,
+          :tshirt_option_id (get
+                             (db/get-option-for-type-and-name
+                              {:type_id id,
+                               :option_name (get params (keyword (str/lower-case tshirt_option_type_name)))})
+                             :id),
+          :tshirt_option_value (if-let [custom-image
+                                        (if (= tshirt_option_type_name "Image")
+                                          (if (= (get params :image) "Custom")
+                                            (get params :custom_image_url)))]
+                                 custom-image
+                                 (get
+                                  (db/get-option-for-type-and-name
+                                   {:type_id id,
+                                    :option_name (get params (keyword (str/lower-case tshirt_option_type_name)))})
+                                  :tshirt_option_value))})
+        (let [option_id
+              (get
+               (db/get-option-for-type-and-name
+                {:type_id id,
+                 :option_name (get params (keyword (str/lower-case tshirt_option_type_name)))})
+               :id)]
+          (if-let [stock
+                   (get
+                    (db/get-stock-for-option-id
+                     {:option_id option_id})
+                    :stock_count)]
+            (db/update-stock-for-option-id!
+             {:stock_count (-
+                            stock
+                            (Integer/parseInt (get params :quantity))),
+              :option_id option_id}))))
+
+      (assoc (response/found "/order") :flash (assoc params :success "true")))))
 
 (defn order-routes []
   [""
